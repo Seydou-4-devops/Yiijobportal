3abd59a4e531a47f344d5dc2ad0ef0d5612364cf
Seydou-4-devops <wazaseyd@gmail.com>
2023-06-05 13:21:33 +0000

    add files to repo

---
+++ backend/config/main-local.php
@@ -0,0 +1,29 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'KdVeujEQY-waiMs55kY0IDCs5caEqUpU',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => \yii\debug\Module::class,
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => \yii\gii\Module::class,
+        'allowedIPs' => ['*'],
+
+        // or allow localhost only
+        // 'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
